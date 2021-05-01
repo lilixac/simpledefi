58b6db95c86719aca9741ca0d007072e646ed6df
lilixac <hinewton25@gmail.com>
2021-05-01 08:10:30 +0545

    Implemented functions to get Dai and Mai balance for selected address

--- client/src/App.js
+++ client/src/App.js
@@ -1,73 +1,98 @@
-import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import React, { useState, useEffect } from "react";
+import TokenFarmContract from "./contracts/TokenFarm.json";
+import DaiTokenContract from "./contracts/DaiToken.json";
+import MyTokenContract from "./contracts/MyToken.json";
 import getWeb3 from "./getWeb3";
 
 import "./App.css";
 
-class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+const App = () => {
+  const [web3, setWeb3] = useState(null);
+  const [account, setAccount] = useState(null);
+  const [tokenFarm, setTokenFarm] = useState(null);
+  const [daiToken, setDaiToken] = useState(null);
+  const [maiToken, setMaiToken] = useState(null);
+  const [daiBalance, setDaiBalance] = useState(0);
+  const [maiBalance, setMaiBalance] = useState(0);
 
-  componentDidMount = async () => {
-    try {
-      // Get network provider and web3 instance.
-      const web3 = await getWeb3();
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        // Get network provider and web3 instance.
+        const web3 = await getWeb3();
+        setWeb3(web3);
 
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
+        // Use web3 to get the user's accounts.
+        const accounts = await web3.eth.getAccounts();
+        setAccount(accounts[0]);
 
-      // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
-      );
+        // Get the contract instance.
+        const networkId = await web3.eth.net.getId();
+        const deployedNetwork = TokenFarmContract.networks[networkId];
+        const instance = new web3.eth.Contract(
+          TokenFarmContract.abi,
+          deployedNetwork && deployedNetwork.address
+        );
+        setTokenFarm(instance);
 
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
-    } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
-      );
-      console.error(error);
+        const daiTokenNetwork = DaiTokenContract.networks[networkId];
+        const daiInstance = new web3.eth.Contract(
+          DaiTokenContract.abi,
+          daiTokenNetwork && daiTokenNetwork.address
+        );
+        setDaiToken(daiInstance);
+
+        const maiTokenNetwork = MyTokenContract.networks[networkId];
+        const maiInstance = new web3.eth.Contract(
+          MyTokenContract.abi,
+          maiTokenNetwork && maiTokenNetwork.address
+        );
+        setMaiToken(maiInstance);
+
+      } catch (error) {
+        // Catch any errors for any of the above operations.
+        alert(
+          `Failed to load web3, accounts, or contract. Check console for details.`
+        );
+        console.error(error);
+      }
     }
+    fetchData();
+  }, []);
+
+  const getDaiBalance = async () => {
+    var daiBalance = await daiToken.methods
+      .balanceOf(account)
+      .call();
+    setDaiBalance(daiBalance.toString());
   };
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
-
-    // Stores a given value, 5 by default.
-    await contract.methods.set(20).send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
+  const getMaiBalance = async () => {
+    var maiBalance = await maiToken.methods
+      .balanceOf(account)
+      .call();
+    setMaiBalance(maiBalance.toString());
   };
 
-  render() {
-    if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
-    }
-    return (
-      <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      {!web3 ? (
+        <div>Loading Web3, accounts, and contract...</div>
+      ) : (
+        <div className="App">
+          <p>{account}</p>
+          <br />
+          <button onClick={getMaiBalance}> Get Mai Token Balance </button>
+          <br/>
+          <button onClick={getDaiBalance}> Get Dai Token Balance </button>
+          <br/>
+          Current Dai balanceof this account: {daiBalance}
+          <br />
+          Current Mai balanceof this account: {maiBalance}
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default App;
